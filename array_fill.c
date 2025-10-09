b01186eeff54773ac5da8fd7004ef7946d2c5652
Quinn Blood <bloodq@wit.edu>
2025-10-09 15:15:22 -0400

    Completed  array_fill.c

--- array_fill.c
+++ array_fill.c
@@ -1,8 +1,17 @@
 #include <stddef.h> // For NULL
 
 /**
- * TODO: Describe what the function does
+ * Fill array with numbers from begin to end
  */
 int array_fill(int* array, int array_len, int begin, int end) {
-    return 1;
+    if(array == NULL || array_len <= 0){
+        return 0;
+    }
+
+    int count = 0;
+    for (int i = begin; i <= end && count < array_len; i++){
+        array[count++] = i;
+    }
+
+    return count;
 }
