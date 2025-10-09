7e38ec03f55da632ea81fabc16afbea71f0ef041
Quinn Blood <bloodq@wit.edu>
2025-10-09 15:16:58 -0400

    Completed  valid_password.c

--- valid_password.c
+++ valid_password.c
@@ -1,9 +1,24 @@
 #include <stddef.h> // For NULL
-#include <stdbool.h> // For true/false
+#include <stdbool.h>
+#include <ctype.h>
 
 /**
- * TODO: Describe what the function does
+ * Sets rules for minimum password requirements
  */
 bool valid_password(const char* s) {
+    if (s == NULL) return false;
+
+    int len = 0, lower = 0, upper = 0;
+    for (int i = 0; s[i] != '\0'; i++) {
+        char c = s[i];
+        if (!isalnum(c)) return false;
+        if (islower(c)) lower++;
+        if (isupper(c)) upper++;
+        len++;
+    }
+
+    if (len < 6 || len > 10) return false;
+    if (lower < 2 || upper < 2) return false;
+
     return true;
 }
