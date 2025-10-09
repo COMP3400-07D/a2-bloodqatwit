1801eb205ad6a67dd2632a3f4a29a66f2145e1ea
Quinn Blood <bloodq@wit.edu>
2025-10-09 15:16:28 -0400

    Completed  string_fill.c

--- string_fill.c
+++ string_fill.c
@@ -1,7 +1,14 @@
 #include <stddef.h> // For NULL
 
 /**
- * TODO: Describe what the function does
+ * fill dest with repeated characters
  */
 void string_fill(char dest[], int dest_cap, char c) {
+    if (dest == NULL || dest_cap <= 0) return;
+
+    int fill_len = dest_cap - 1;
+    for (int i = 0; i < fill_len; i++) {
+        dest[i] = c;
+    }
+    dest[fill_len] = '\0';
 }
